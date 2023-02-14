c2dfae26e5fe5104a0fa229585f9496decd71195
adi nurr <adinurr@gmail.com>
2023-02-14 19:22:04 +0700

    Add unit tests

---
+++ UnitTests/UserRepositoriesTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using Web.Core.Sport.Organize.Repositories;
+using Web.Core.Sport.Organize.Services;
+
+namespace UnitTests
+{
+    public class UserRepositoriesTests
+    {
+        UserRepositories userRepositories;
+        GlobalService globalService; 
+
+        [SetUp]
+        public void Setup()
+        {
+            globalService = new GlobalService();
+            userRepositories = new UserRepositories(globalService);
+        }
+        [Test]
+        public void GetUser_Work()
+        {
+            //Arrange
+            var id = 1;
+            var token = " xxxxx";
+
+            //Act
+            var data = userRepositories.GetUser(id,token);
+
+            //Assert
+            Assert.That(data, Is.EqualTo(null));
+        }
+    }
+}
